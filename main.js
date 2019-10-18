064174a64c663bfe093e6ab4a4e0373aea6a9986
rummolprod999 <rummolprod999@gmail.com>
2019-10-18 11:06:08 +0300

    update

--- main.js
+++ main.js
@@ -17,13 +17,14 @@ function createWindow () {
     });
 
     // and load the index.html of the app.
+    mainWindow.webContents.openDevTools();
     mainWindow.loadFile('index.html');
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function () {
+    mainWindow.on('closed', () => {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
@@ -37,7 +38,7 @@ function createWindow () {
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') app.quit()
